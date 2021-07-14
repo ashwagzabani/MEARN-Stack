011234524e8c5303bceaacca461d44c0c6786048
ashwag zabani <ashwag.amz@gmail.com>
2021-07-14 14:35:46 +0300

    fix the fake path of image url and test show image uploaded

--- js/main.js
+++ js/main.js
@@ -9,33 +9,71 @@ console.log(a.blogs);
 
 
 function createNewBlog() {
-    var blogTitle = document.getElementById('blogTitle').value
-    var blogContent = document.getElementById('blogContent').value
-    var blogImg = document.getElementById('blogImg').value
-    // console.log("object " + blogImg + ' ' + blogContent);
+    var blogTitle = document.getElementById('blogTitle').value;
+    var blogContent = document.getElementById('blogContent').value;
+
     $('#newBlogForm').modal('hide')
-    newBlog(blogTitle, blogContent, blogImg);
+    newBlog(blogTitle, blogContent);
 
 }
 
-function newBlog(blogTitle, blogContent, blogImg) {
+// convert file to a base64 url
+const readURL = file => {
+    return new Promise((res, rej) => {
+        const reader = new FileReader();
+        reader.onload = e => res(e.target.result);
+        reader.onerror = e => rej(e);
+        reader.readAsDataURL(file);
+    });
+};
+
+const onFileSelected = async event => {
+    const file = event.target.files[0];
+    const url = await readURL(file);
+    localStorage.setItem('tempImgUrl', url)
+    return url;
+
+};
+
+function newBlog(blogTitle, blogContent) {
     var newBlogObj = {
         id: a.blogs.length + 1,
         title: blogTitle,
         content: blogContent,
-        image: blogImg
+        image: localStorage.getItem('tempImgUrl')
     }
     currentBlogs.push(newBlogObj);
     localStorage.setItem('blogDB', JSON.stringify({ "blogs": currentBlogs }))
+
     createNewBlogDOM(newBlogObj.id, blogTitle, blogContent, blogImg)
 
 }
 
-function createNewBlogDOM(blogId, blogTitle, blogContent, blogImg) {
+
+function createNewBlogDOM(blogId, blogTitle, blogContent) {
     var parent = document.getElementById('blogCards');
-    var card = document.createElement('div').classList.add("card");
-    var card_header = document.createElement('div').classList.add("card-header");;
-    parent.append(card)
-    card.append(card_header)
+    let card = document.createElement('div');
+    card.className = 'card';
+
+    let cardHeader = document.createElement('div');
+    cardHeader.className = 'card-header';
+
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    let img = document.createElement('img');
+    img.src = localStorage.getItem('tempImgUrl');
+
+
+    let title = document.createElement('h5');
+    title.innerText = "task.title";
+    title.className = 'card-title';
+
+
+    cardBody.appendChild(title);
+    cardHeader.appendChild(img);
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    parent.appendChild(card);
     console.log("hnjmkl");
 }
